c69061fb6a5b4c6cda46ca510ba3d5ba87ece373
michal dub <michal.dub@sanomamedia.cz>
2014-06-23 14:31:49 +0200

    fixed django 1.6 support

--- ella_contests/urls.py
+++ ella_contests/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls.defaults import patterns, url
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
+
+try:
+    from django.conf.urls import patterns, url
+except ImportError:  # Django < 1.4
+    from django.conf.urls.defaults import patterns, url
 
 from ella_contests.views import (
                                  contest_result,
--- test_ella_contests/urls.py
+++ test_ella_contests/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls.defaults import patterns, url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError:  # Django < 1.4
+    from django.conf.urls.defaults import patterns, url, include
 
 admin.autodiscover()
 
