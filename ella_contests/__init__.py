e39143231e9380f965a8c26fca10c8c6817aafc7
michal dub <michal.dub@sanomamedia.cz>
2015-07-22 15:04:54 +0200

    added app config

--- ella_contests/__init__.py
+++ ella_contests/__init__.py
@@ -1,3 +1,5 @@
 VERSION = (0, 0, 7)
 __version__ = VERSION
 __versionstr__ = ".".join(map(str, VERSION))
+
+default_app_config = 'ella_contests.apps.EllaContestsConfig'
---
+++ ella_contests/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class EllaContestsConfig(AppConfig):
+    name = 'ella_contests'
+    verbose_name = _("Contests")
